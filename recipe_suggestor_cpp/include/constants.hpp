ccf422ff95d6380d5726cf3b38726ab242f34124
SalvatoreLS <losardosalvatorejr@gmail.com>
2026-01-23 18:51:53 +0100

    Added constants.hpp and types.hpp

---
+++ recipe_suggestor_cpp/include/constants.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace constants {
+
+    // Detector constants
+    inline constexpr float default_conf_threshold = 0.5f;
+    inline constexpr float nms_threshold = 0.45f;
+    inline constexpr int sort_threshold = 20;
+    inline constexpr float pixel_scale = 1.0f / 255.0f;
+    inline constexpr int intra_op_num_threads = 1;
+
+    // Router constants
+    inline constexpr double crop_width_factor = 0.35;
+    inline constexpr double crop_height_factor = 0.18;
+    inline constexpr double crop_start_x_factor = 0.97;
+
+    // Trie constants
+    inline constexpr int max_elements_rank = 15;
+}
---
+++ recipe_suggestor_cpp/include/types.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <sys/types.h>
+
+namespace types {
+    using ItemID = u_int16_t;
+    using ConsumableID = u_int16_t;
+    using Quantity = u_int8_t;
+    using FrameCount = u_int32_t;
+}
